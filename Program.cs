47853f09ed88fec3c4e691778830bfa2e5619a8d
Fiyaz007 <fiyaznazeer007@gmail.com>
2024-10-07 09:31:07 +0530

    Update 1

--- Program.cs
+++ Program.cs
@@ -10,7 +10,22 @@ namespace SampleConsoleApp
     {
         static void Main(string[] args)
         {
-            
+            //Program to print a pattern
+            for (int i = 1; i <= 11; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Enter the no of row: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
